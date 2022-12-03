02b9f3335e52eead443822f5b9a2408422b1999e
Dark Session <webmaster@revolution-gaming-community.com>
2022-12-03 22:10:08 +0100

    EDDNProcessor: Improved exception handling

--- src/EDDataProcessor/EDDN/EDDNProcessor.cs
+++ src/EDDataProcessor/EDDN/EDDNProcessor.cs
@@ -28,49 +28,60 @@ namespace EDDataProcessor.EDDN
 
         public async Task ProcessMessages(CancellationToken cancellationToken = default)
         {
-            Endpoint activeMqEndpont = Endpoint.Create(
-                Configuration.GetValue<string>("ActiveMQ:Host") ?? throw new Exception("No ActiveMQ host configured"),
-                Configuration.GetValue<int>("ActiveMQ:Port"),
-                Configuration.GetValue<string>("ActiveMQ:Username") ?? string.Empty,
-                Configuration.GetValue<string>("ActiveMQ:Password") ?? string.Empty);
+            Log.LogInformation("Starting EDDNProcessor");
 
-            ConnectionFactory connectionFactory = new();
-            await using IConnection connection = await connectionFactory.CreateAsync(activeMqEndpont, cancellationToken);
-            await using IConsumer consumer = await connection.CreateConsumerAsync("EDDN", RoutingType.Anycast, cancellationToken);
-            await using IAnonymousProducer anonymousProducer = await connection.CreateAnonymousProducerAsync(cancellationToken);
-
-            while (!cancellationToken.IsCancellationRequested)
+            try
             {
-                try
+                Endpoint activeMqEndpont = Endpoint.Create(
+                    Configuration.GetValue<string>("ActiveMQ:Host") ?? throw new Exception("No ActiveMQ host configured"),
+                    Configuration.GetValue<int>("ActiveMQ:Port"),
+                    Configuration.GetValue<string>("ActiveMQ:Username") ?? string.Empty,
+                    Configuration.GetValue<string>("ActiveMQ:Password") ?? string.Empty);
+
+                ConnectionFactory connectionFactory = new();
+                await using IConnection connection = await connectionFactory.CreateAsync(activeMqEndpont, cancellationToken);
+                await using IConsumer consumer = await connection.CreateConsumerAsync("EDDN", RoutingType.Anycast, cancellationToken);
+                await using IAnonymousProducer anonymousProducer = await connection.CreateAnonymousProducerAsync(cancellationToken);
+
+                Log.LogInformation("EDDNProcessor: Waiting for messages...");
+
+                while (!cancellationToken.IsCancellationRequested)
                 {
-                    Message message = await consumer.ReceiveAsync(cancellationToken);
-                    await using Transaction transaction = new();
-                    await consumer.AcceptAsync(message, transaction, cancellationToken);
-                    string jsonString = message.GetBody<string>();
-                    JObject json = JObject.Parse(jsonString);
-                    if (json.TryGetValue("$schemaRef", out JToken? schemaRef) &&
-                        schemaRef.Value<string>() is string schema &&
-                        !string.IsNullOrEmpty(schema) &&
-                        EDDNProcessors.TryGetValue(schema, out Type? schemaClass) &&
-                        json.ToObject(schemaClass) is IEDDNEvent eddnEvent)
+                    try
                     {
-                        using IServiceScope serviceScope = ServiceProvider.CreateScope();
-                        EdDbContext dbContext = serviceScope.ServiceProvider.GetRequiredService<EdDbContext>();
-                        if (!await dbContext.Database.CanConnectAsync(cancellationToken))
+                        Message message = await consumer.ReceiveAsync(cancellationToken);
+                        await using Transaction transaction = new();
+                        await consumer.AcceptAsync(message, transaction, cancellationToken);
+                        string jsonString = message.GetBody<string>();
+                        JObject json = JObject.Parse(jsonString);
+                        if (json.TryGetValue("$schemaRef", out JToken? schemaRef) &&
+                            schemaRef.Value<string>() is string schema &&
+                            !string.IsNullOrEmpty(schema) &&
+                            EDDNProcessors.TryGetValue(schema, out Type? schemaClass) &&
+                            json.ToObject(schemaClass) is IEDDNEvent eddnEvent)
                         {
-                            Log.LogError("Unable to connect to database...");
-                            await transaction.RollbackAsync(cancellationToken);
-                            await Task.Delay(TimeSpan.FromSeconds(30), cancellationToken);
+                            using IServiceScope serviceScope = ServiceProvider.CreateScope();
+                            EdDbContext dbContext = serviceScope.ServiceProvider.GetRequiredService<EdDbContext>();
+                            if (!await dbContext.Database.CanConnectAsync(cancellationToken))
+                            {
+                                Log.LogError("Unable to connect to database...");
+                                await transaction.RollbackAsync(cancellationToken);
+                                await Task.Delay(TimeSpan.FromSeconds(30), cancellationToken);
+                            }
+                            await eddnEvent.ProcessEvent(dbContext, anonymousProducer, transaction, cancellationToken);
                         }
-                        await eddnEvent.ProcessEvent(dbContext, anonymousProducer, transaction, cancellationToken);
+                        await transaction.CommitAsync(cancellationToken);
                     }
-                    await transaction.CommitAsync(cancellationToken);
+                    catch (Exception e)
+                    {
+                        Log.LogError(e, "Processing message exception");
+                        await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+                    }
                 }
-                catch (Exception e)
-                {
-                    Log.LogError(e, "Processing message exception");
-                    await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
-                }
+            }
+            catch (Exception e)
+            {
+                Log.LogError(e, "EDDNProcessor exception");
             }
         }
     }
