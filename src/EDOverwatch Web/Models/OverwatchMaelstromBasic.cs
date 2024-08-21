3d496d83a47bb53f1a698cb9448b85e04706919e
Dark Session <68336675+DarkSession@users.noreply.github.com>
2024-08-21 19:00:47 +0200

    Remove CausticLevel duplicate

--- src/EDOverwatch Web/Models/OverwatchMaelstromBasic.cs
+++ src/EDOverwatch Web/Models/OverwatchMaelstromBasic.cs
@@ -1,7 +1,4 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-
-namespace EDOverwatch_Web.Models
+﻿namespace EDOverwatch_Web.Models
 {
     public class OverwatchMaelstromBasic : OverwatchMaelstromProgress
     {
@@ -11,8 +8,6 @@ namespace EDOverwatch_Web.Models
         public int SystemsInRecovery { get; }
         public decimal DefenseRate { get; }
         public TitanDamageResistance DamageResistance { get; }
-        [JsonConverter(typeof(StringEnumConverter))]
-        public TitanCausticLevel CausticLevel { get; }
 
         public OverwatchMaelstromBasic(
             ThargoidMaelstrom thargoidMaelstrom,
@@ -35,22 +30,6 @@ namespace EDOverwatch_Web.Models
                 DefenseRate = Math.Round((decimal)(populatedInvasionsDefended + populatedAlertsDefended) / (decimal)invasionsAlertsTotal, 4);
             }
             DamageResistance = TitanDamageResistance.GetDamageResistance(systemsThargoidControlled, thargoidMaelstrom.HeartsRemaining);
-            CausticLevel = TitanCausticLevel.None;
-            if (thargoidMaelstrom.DefeatCycle is not null && thargoidMaelstrom.MeltdownTimeEstimate is not null && thargoidMaelstrom.MeltdownTimeEstimate <= DateTimeOffset.UtcNow)
-            {
-                var ticksSinceDefated = WeeklyTick.GetNumberOfTicksSinceDate(thargoidMaelstrom.DefeatCycle.Start);
-                if (ticksSinceDefated >= 0)
-                {
-                    CausticLevel = ticksSinceDefated switch
-                    {
-                        < 0 => TitanCausticLevel.None,
-                        <= 1 => TitanCausticLevel.Extreme,
-                        <= 2 => TitanCausticLevel.Medium,
-                        <= 3 => TitanCausticLevel.Low,
-                        _ => TitanCausticLevel.None,
-                    };
-                }
-            }
         }
     }
 }
